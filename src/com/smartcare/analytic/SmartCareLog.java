1a6c46f632916807cec8c0e91ef14bca15d5237a
pradeep <pradeep.vasudeva@gmail.com>
2015-02-23 14:28:52 -0800

    ..

--- src/com/smartcare/analytic/SmartCareLog.java
+++ src/com/smartcare/analytic/SmartCareLog.java
@@ -1,5 +1,26 @@
 package com.smartcare.analytic;
 
 public class SmartCareLog {
-
+	private String description;
+	private String error;
+	private String createdDate;
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+	public String getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
 }
